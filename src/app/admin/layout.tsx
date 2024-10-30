42dbba4cde220eadab0f263aa689745ad448f70c
owlicodes <owlicodes@gmail.com>
2024-10-30 14:53:56 +0800

    Creating the admin layout with sidebar component.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,20 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Sidebar } from "@/features/common/components/sidebar";
+
 type AdminLayoutProps = {
   children: React.ReactNode;
 };
 
 export default function AdminLayout({ children }: AdminLayoutProps) {
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider>
+      <Sidebar />
+
+      <div>
+        <SidebarTrigger />
+
+        <div className="w-full p-4">{children}</div>
+      </div>
+    </SidebarProvider>
+  );
 }
---
+++ src/features/common/components/sidebar.tsx
@@ -0,0 +1,51 @@
+import { Home, LayoutDashboard } from "lucide-react";
+
+import {
+  Sidebar as ShadSidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Dashboard",
+    url: "/admin/dashboard",
+    icon: LayoutDashboard,
+  },
+];
+
+export function Sidebar() {
+  return (
+    <ShadSidebar>
+      <SidebarContent className="bg-secondary">
+        <SidebarGroup>
+          <SidebarGroupLabel>Owlistate Admin</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </ShadSidebar>
+  );
+}
